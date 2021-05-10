5139343c38fe82b5b1c73c3f0b6b5beae0a0a38a
VINOTH-DEV01 <kumar434vinoth@gmail.com>
2021-05-10 19:46:29 +0530

    few changes are done! welcome message is added now!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to pickup , its platform to pickup your own mentors. (by vinoth).
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to pickup , its platform to pickup your own mentors
         </a>
       </header>
     </div>
